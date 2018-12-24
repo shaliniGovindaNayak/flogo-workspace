e47ecf9f9db95629bdee89185fb1ca558f974d2f
shaliniGovindaNayak <shalini.n@isteer.com>
2018-12-24 12:25:50 +0530

    Adds addition activity

---
+++ activity/addition/activity.go
@@ -0,0 +1,34 @@
+package hello
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+var log = logger.GetLogger("activity-helloworld")
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+
+	num1 := context.GetInput("num1").(int)
+	num2 := context.GetInput("num2").(int)
+
+	context.SetOutput("output", num1+num2)
+
+	return true, nil
+}
---
+++ activity/addition/activity_test.go
@@ -0,0 +1,61 @@
+package hello
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func TestCreate(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+}
+
+func TestEval(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	tc.SetInput("num1", 10)
+	tc.SetInput("num2", 20)
+
+	act.Eval(tc)
+
+	fmt.Println(tc.GetOutput("output"))
+
+	//assert.Equal(t, result, "The Flogo engine says Hello to Flogo Dev")
+	//check result attr
+}
