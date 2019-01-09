3548b7a2c118b82a29e741bf7a0de954af371a43
ShaliniGovindaNayak <shalini.n@isteer.com>
2019-01-09 17:37:44 +0530

    adds a new activity

--- activity/stringtojson/activity.go
+++ activity/stringtojson/activity.go
@@ -3,6 +3,9 @@ package stringtojson
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"strings"
 
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 )
@@ -28,18 +31,18 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	input := context.GetInput("Rawstring").(string)
 	println(input)
 
-	type Data struct {
-		temp  string
-		humid string
+	type Message struct {
+		Temp, Humid string
 	}
-	var datas Data
-	err = json.Unmarshal([]byte(input), &datas)
-	if err != nil {
-		fmt.Println("error:", err)
+	dec := json.NewDecoder(strings.NewReader(input))
+	for {
+		var m Message
+		if err := dec.Decode(&m); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("temparature:%s, humidity:%s\n", m.Temp, m.Humid)
 	}
-	fmt.Printf("%+v", datas)
-	fmt.Println()
-	context.SetOutput("Json", datas)
-
 	return true, nil
 }
