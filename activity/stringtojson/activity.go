5ba7ef87f39a9e620bdafbde1918e7d5578918ac
ShaliniGovindaNayak <shalini.n@isteer.com>
2019-01-09 18:32:37 +0530

    adds a new activity

--- activity/stringtojson/activity.go
+++ activity/stringtojson/activity.go
@@ -34,8 +34,8 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
 	println(&raw)
 	raw["count"] = 1
-	out, _ := json.Marshal(&raw)
-	context.SetOutput("Json", out)
-	println(string(out))
+	//out, _ := json.Marshal(&raw)
+
+	context.SetOutput("Json", &raw)
 	return true, nil
 }
