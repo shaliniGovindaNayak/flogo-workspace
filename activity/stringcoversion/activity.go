1177c053256836240af0fdd54fcde11d4229941a
ShaliniGovindaNayak <45679496+shaliniGovindaNayak@users.noreply.github.com>
2019-01-10 18:54:10 +0530

    Add files via upload

--- activity/stringcoversion/activity.go
+++ activity/stringcoversion/activity.go
@@ -30,7 +30,7 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	if s, err := strconv.ParseFloat(input, 32); err == nil {
 		fmt.Printf("%T, %v\n", s, s)
 	}
-	context.SetOutput("Float", s)
+	context.SetOutput("Float32", s)
 
 	return true, nil
 }
