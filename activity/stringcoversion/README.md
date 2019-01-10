2e534ad40e401e4fc01fc5611084c0d06fea8089
ShaliniGovindaNayak <45679496+shaliniGovindaNayak@users.noreply.github.com>
2019-01-10 18:35:46 +0530

    Add files via upload

--- activity/stringcoversion/README.md
+++ activity/stringcoversion/README.md
@@ -1,1 +1,2 @@
 
+# 	stringconversion - Activity
---
+++ activity/stringcoversion/activity.go
@@ -0,0 +1,36 @@
+package stringconversion
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+)
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+
+	input := context.GetInput("String").(string)
+	var s float32
+	if s, err := strconv.ParseFloat(input, 32); err == nil {
+		fmt.Printf("%T, %v\n", s, s)
+	}
+	context.SetOutput("Float", s)
+
+	return true, nil
+}
