9a36a18e5644d87da7d1fec1565f8df34cc2e790
ShaliniGovindaNayak <shalini.n@isteer.com>
2019-01-30 18:31:17 +0530

    changes

--- activity/neo4j/activity.go
+++ activity/neo4j/activity.go
@@ -27,8 +27,8 @@ func (a *MyActivity) Metadata() *activity.Metadata {
 func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
 	driver := bolt.NewDriver()
-	conn, _ := driver.OpenNeo("bolt://192.168.1.34:7687")
-	defer conn.Close()
+	conn, _ := driver.OpenNeo("bolt://localhost:7687")
+	//defer conn.Close()
 
 	result, _ := conn.ExecNeo("CREATE (n:NODE {foo: {foo}, bar: {bar}})", map[string]interface{}{"foo": 1, "bar": 2.2})
 	numResult, _ := result.RowsAffected()
