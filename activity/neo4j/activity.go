6d9bbe5ed0365e737c92ac93460eda50ece8c59d
ShaliniGovindaNayak <shalini.n@isteer.com>
2019-01-30 18:14:10 +0530

    more changes

--- activity/neo4j/activity.go
+++ activity/neo4j/activity.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 	bolt "github.com/johnnadratowski/golang-neo4j-bolt-driver"
+	"github.com/johnnadratowski/golang-neo4j-bolt-driver/structures/graph"
 )
 
 // MyActivity is a stub for your Activity implementation
@@ -26,31 +27,36 @@ func (a *MyActivity) Metadata() *activity.Metadata {
 func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
 	driver := bolt.NewDriver()
-	conn, err := driver.OpenNeo("bolt://192.168.1.34:7687")
-	if err != nil {
-		panic(err)
-	}
+	conn, _ := driver.OpenNeo("bolt://localhost:7687")
 	defer conn.Close()
 
-	stmt, err := conn.PrepareNeo("CREATE (n:NODE {foo: {foo}, bar: {bar}})")
-	if err != nil {
-		panic(err)
-	}
-
-	// Executing a statement just returns summary information
-	result, err := stmt.ExecNeo(map[string]interface{}{"foo": 1, "bar": 2.2})
-	if err != nil {
-		panic(err)
-	}
-	numResult, err := result.RowsAffected()
-	if err != nil {
-		panic(err)
-	}
+	result, _ := conn.ExecNeo("CREATE (n:NODE {foo: {foo}, bar: {bar}})", map[string]interface{}{"foo": 1, "bar": 2.2})
+	numResult, _ := result.RowsAffected()
 	fmt.Printf("CREATED ROWS: %d\n", numResult) // CREATED ROWS: 1
 
-	context.SetOutput("output", numResult)
-	// Closing the statment will also close the rows
-	stmt.Close()
+	// Lets get the node
+	data, rowsMetadata, _, _ := conn.QueryNeoAll("MATCH (n:NODE) RETURN n.foo, n.bar", nil)
+	fmt.Printf("COLUMNS: %#v\n", rowsMetadata["fields"].([]interface{})) // COLUMNS: n.foo,n.bar
+	fmt.Printf("FIELDS: %d %f\n", data[0][0].(int64), data[0][1].(float64))
+
+	// in neo concurrently with a pipeline
+	results, _ := conn.ExecPipeline([]string{
+		"MATCH (n:NODE) CREATE (n)-[:REL]->(f:FOO)",
+		"MATCH (n:NODE) CREATE (n)-[:REL]->(b:BAR)",
+		"MATCH (n:NODE) CREATE (n)-[:REL]->(z:BAZ)",
+		"MATCH (n:NODE) CREATE (n)-[:REL]->(f:FOO)",
+		"MATCH (n:NODE) CREATE (n)-[:REL]->(b:BAR)",
+		"MATCH (n:NODE) CREATE (n)-[:REL]->(z:BAZ)",
+	}, nil, nil, nil, nil, nil, nil)
+	for _, result := range results {
+		numResult, _ := result.RowsAffected()
+		fmt.Printf("CREATED ROWS: %d\n", numResult) // CREATED ROWS: 2 (per each iteration)
+	}
+
+	data, _, _, _ = conn.QueryNeoAll("MATCH (n:NODE)-[:REL]->(m) RETURN m", nil)
+	for _, row := range data {
+		fmt.Printf("NODE: %#v\n", row[0].(graph.Node)) // Prints all nodes
+	}
 
 	return true, nil
 }
