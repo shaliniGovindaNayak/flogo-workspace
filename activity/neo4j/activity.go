c19acc61aee635beb93cbbfbfb69e7ecb5b010a6
ShaliniGovindaNayak <shalini.n@isteer.com>
2019-01-31 15:12:42 +0530

    ip

--- activity/neo4j/activity.go
+++ activity/neo4j/activity.go
@@ -31,7 +31,7 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
 	fmt.Println(url)
 	driver := bolt.NewDriver()
-	conn, _ := driver.OpenNeo("bolt://localhost:7687")
+	conn, _ := driver.OpenNeo("bolt://192.168.1.34:7687")
 	defer conn.Close()
 
 	// Start by creating a node
