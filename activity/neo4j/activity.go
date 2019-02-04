24ed67a65cfac27c2904cbf855b5d52a8bf67e1e
ShaliniGovindaNayak <shalini.n@isteer.com>
2019-02-04 16:10:33 +0530

    new activity

--- activity/neo4j/activity.go
+++ activity/neo4j/activity.go
@@ -1,7 +1,11 @@
 package neo4j
 
 import (
+	"log"
+
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+
+	bolt "github.com/johnnadratowski/golang-neo4j-bolt-driver"
 )
 
 // MyActivity is a stub for your Activity implementation
@@ -20,9 +24,22 @@ func (a *MyActivity) Metadata() *activity.Metadata {
 }
 
 // Eval implements activity.Activity.Eval
-func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
-	// do eval
+	url := context.GetInput("url").(string)
+	driver := bolt.NewDriver()
+	log.Println("created new driver")
+	conn, err := driver.Open(url)
+	if err != nil {
+		log.Println("error while trying to connect to bolt")
+	}
+	result, err := conn.Prepare("create (n:node) return n")
+	if err != nil {
+		log.Println("error while executing the query")
+	}
+
+	log.Println("output:", result)
+	context.SetOutput("output", result)
 
 	return true, nil
 }
