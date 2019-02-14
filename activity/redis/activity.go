e7d6870aa6be730c1fa88aeaf57901f2eec123b9
ShaliniGovindaNayak <shalini.n@isteer.com>
2019-02-14 16:20:25 +0530

    hashing

--- activity/redis/activity.go
+++ activity/redis/activity.go
@@ -20,8 +20,32 @@ func (a *MyActivity) Metadata() *activity.Metadata {
 	return a.metadata
 }
 
+//var redis *miniredis
 // Eval implements activity.Activity.Eval
 func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+
+	key := context.GetInput("key").(string)
+	value := context.GetInput("value").(string)
+	field := context.GetInput("field").(string)
+	operation := context.GetInput("operation").(string)
+	var result string
+
+	switch operation {
+
+	case "strings":
+		result = set(key, value)
+		break
+
+	case "hash":
+		result = hash(key, field, value)
+	}
+
+	context.SetOutput("output", result)
+
+	return true, nil
+}
+
+func set(key string, value string) string {
 
 	redis, err := miniredis.Run()
 	if err != nil {
@@ -29,14 +53,22 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	}
 	defer redis.Close()
 
-	key := context.GetInput("key").(string)
-	value := context.GetInput("value").(string)
-	field := context.GetInput("field").(string)
+	redis.Set(key, value)
+	res, _ := redis.Get(key)
+
+	return res
+
+}
+
+func hash(key string, field string, value string) string {
+
+	redis, err := miniredis.Run()
+	if err != nil {
+		panic(err)
+	}
+	defer redis.Close()
 
 	redis.HSet(key, field, value)
-	result := redis.HGet(key, field)
+	return redis.HGet(key, field)
 
-	context.SetOutput("output", result)
-
-	return true, nil
 }
