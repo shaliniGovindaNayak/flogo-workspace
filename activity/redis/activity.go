96000859a94b2ba4228bdd79d65ddbadf0f0b60d
ShaliniGovindaNayak <shalini.n@isteer.com>
2019-02-18 16:11:45 +0530

    changed

--- activity/redis/activity.go
+++ activity/redis/activity.go
@@ -23,76 +23,131 @@ func (a *MyActivity) Metadata() *activity.Metadata {
 // Eval implements activity.Activity.Eval
 func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
+	Commands := context.GetInput("Commands").(string)
+
 	key := context.GetInput("key").(string)
 	value := context.GetInput("value").(string)
-	operation := context.GetInput("operation").(string)
+	incr := context.GetInput("Incr").(int)
+	//decr := context.GetInput("Decr").(int)
 	field := context.GetInput("field").(string)
-	var result string
-	var res []string
+	var res string
 
-	switch operation {
+	redis, err := miniredis.Run()
+	if err != nil {
+		panic(err)
+	}
+	switch Commands {
 
-	case "strings":
-		result = set(key, value)
-		context.SetOutput("output", result)
+	case "TTL":
+		res = string(redis.TTL(key))
 		break
 
-	case "hash":
-		result = hash(key, field, value)
-		context.SetOutput("output", result)
+	//case "SetTTL":
+	//res = setTTl(key)
+
+	case "Type":
+		res = redis.Type(key)
 		break
 
-	case "list":
-		val := []string{"hai", "hello", "bye"}
-		res = list(key, val)
+	case "Dump":
+		res = redis.Dump()
+		break
+
+	case "Del":
+		res := redis.Del(key)
 		context.SetOutput("output", res)
 		break
 
+	//case "DB":
+	//	res = db()
+	//	break
+
+	case "Exists":
+		res := redis.Exists(key)
+		context.SetOutput("output", res)
+		break
+
+	case "Flushall":
+		redis.FlushAll()
+		break
+
+	case "FlushDB":
+		redis.FlushDB()
+		break
+
+	case "Set":
+		redis.Set(key, value)
+		break
+
+	case "Get":
+		res, err = redis.Get(key)
+
+	case "Increment":
+		res, _ := redis.Incr(key, incr)
+		context.SetOutput("output", res)
+
+	case "Hget":
+		res = redis.HGet(key, field)
+		break
+
+	case "Hset":
+		redis.HSet(key, field, value)
+		break
+
+	case "Hdel":
+		redis.HDel(key, field)
+		break
+
+		//	case "Hkeys":
+	//	res := redis.HKeys(key)
+	//		break
+
+	case "Hincrement":
+		res, _ := redis.HIncr(key, field, incr)
+		context.SetOutput("output", res)
+
+		//	case "List":
+		//		res = redis.List(key)
+		//		break
+
+	case "Lpush":
+		res, _ := redis.Lpush(key, value)
+		context.SetOutput("output", res)
+
+	case "Lpop":
+		res, _ = redis.Lpop(key)
+		break
+
+	case "Rpush":
+		res, _ := redis.Push(key, value)
+		context.SetOutput("output", res)
+
+	case "Rpop":
+		res, _ = redis.Pop(key)
+
+	case "Add":
+		res, _ := redis.SetAdd(key, value)
+		context.SetOutput("output", res)
+
+		//	case "Members":
+		//		res = redis.Members(key)
+
+		//	case "Zadd":
+		//		res = redis.ZAdd(key)
+
+	case "Zrem":
+		res, _ := redis.ZRem(key, value)
+		context.SetOutput("output", res)
+
+	case "Zscore":
+		res, _ := redis.ZScore(key, value)
+		context.SetOutput("output", res)
+
+	case "Ismember":
+		res, _ := redis.IsMember(key, value)
+		context.SetOutput("output", res)
 	}
 
+	context.SetOutput("output", res)
 	return true, nil
-}
-
-func set(key string, value string) string {
-
-	redis, err := miniredis.Run()
-	if err != nil {
-		panic(err)
-	}
-	defer redis.Close()
-
-	redis.Set(key, value)
-	res, _ := redis.Get(key)
-
-	return res
-
-}
-
-func hash(key string, field string, value string) string {
-
-	redis, err := miniredis.Run()
-	if err != nil {
-		panic(err)
-	}
-	defer redis.Close()
-
-	redis.HSet(key, field, value)
-	return redis.HGet(key, field)
-
-}
-
-func list(key string, value []string) []string {
-
-	redis, err := miniredis.Run()
-	if err != nil {
-		panic(err)
-	}
-	defer redis.Close()
-
-	var res []string
-	for i := 0; i <= len(value); i++ {
-		redis.Lpush(key, value[i])
-	}
-	res, _ = redis.List(key)
-	return res
 }
