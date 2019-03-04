7ce170513c642d81cdb66b7d021218f96086c1f4
ShaliniGovindaNayak <shalini.n@isteer.com>
2019-03-04 17:49:57 +0530

    publishact

--- activity/publishmqtt/activity.go
+++ activity/publishmqtt/activity.go
@@ -4,7 +4,14 @@ import (
 	"fmt"
 
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	dht "github.com/d2r2/go-dht"
+	logger "github.com/d2r2/go-logger"
 	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+var lg = logger.NewPackageLogger("main",
+	logger.DebugLevel,
+	// logger.InfoLevel,
 )
 
 type params struct {
@@ -43,7 +50,7 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	host := context.GetInput("host").(string)
 	password := context.GetInput("password").(string)
 	username := context.GetInput("username").(string)
-	payload := context.GetInput("payload").(string)
+	//payload := context.GetInput("payload").(string)
 
 	credentails := params{topic, host, password, username, "host", false, 0, 1, "pub", ":memory"}
 	c1 := &credentails
@@ -80,7 +87,40 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 		panic(token.Error())
 	}
 
-	client.Publish(c1.topic, byte(c1.qos), false, payload)
-	context.SetOutput("output", "Done..")
-	return true, nil
+loop:
+	for {
+		fmt.Println("*************************************Sample Publisher Started************************************************")
+
+		fmt.Println("************************************doing publish*******************************************************")
+
+		for payload := range generate() {
+
+			client.Publish(c1.topic, byte(c1.qos), false, payload)
+			//token.Wait()
+			lg.Infof("Published message %s", payload)
+			lg.Infof("done...")
+			context.SetOutput("output", "Done...")
+			continue loop
+		}
+	}
+
+	//return true, nil
+}
+
+func generate() <-chan string {
+	c := make(chan string)
+	go func() {
+		temperature, humidity, retried, err :=
+			dht.ReadDHTxxWithRetry(dht.DHT11, 17, false, 10)
+		if err != nil {
+			lg.Fatal(err)
+		}
+		lg.Infof("Sensor = %v: Temperature = %v*C, Humidity = %v%% (retried %d times)",
+			dht.DHT11, temperature, humidity, retried)
+
+		c <- fmt.Sprintf(`{"temperature": "%v", "humidity": "%v"}`, temperature, humidity)
+
+	}()
+
+	return c
 }
