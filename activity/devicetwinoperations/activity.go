331838fc31e8581266c1175818ae04d80ba91cc3
ShaliniGovindaNayak <shalini.n@isteer.com>
2019-04-05 12:37:37 +0530

    added changes

--- activity/devicetwinoperations/activity.go
+++ activity/devicetwinoperations/activity.go
@@ -25,7 +25,7 @@ const (
 	ivconnectionString = "connectionString"
 	ivTypeofOp         = "Type of Operation"
 	ivDeviceID         = "Device ID"
-	ivJsonDoc          = "Json Document"
+	//ivJsonDoc          = "Json Document"
 
 	ovResult = "result"
 	ovStatus = "status"
@@ -71,7 +71,7 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	connectionString := context.GetInput(ivconnectionString).(string)
 	methodType := context.GetInput(ivTypeofOp).(string)
 	deviceID := context.GetInput(ivDeviceID).(string)
-	jsonDoc := context.GetInput(ivJsonDoc).(string)
+	jsonDoc := context.GetInput("Json Document").(string)
 
 	log.Debug("The connection string to device is [%s]", connectionString)
 	log.Debug("The Method type selected is [%s]", methodType)
