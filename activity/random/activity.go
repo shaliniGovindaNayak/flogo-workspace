745342f4e5126fd295b4b0f2af5105b124404425
Shalini G Nayak <shalini.n@isteer.com>
2019-07-19 18:02:17 +0530

    time updates

--- activity/random/activity.go
+++ activity/random/activity.go
@@ -29,7 +29,7 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
 	array := []int{1, 3, 4, 5, 6, 7, 8, 9}
 	array2 := [][]int{{2, 3}, {8, 9}, {11, 12}, {14, 15, 16, 29, 30}, {17, 18}, {20, 21}, {23, 24, 25, 31, 32}, {26, 27, 28, 36, 34}}
-	array3 := []int{1, 2, 3, 4, 5}
+	array3 := []int{14, 15, 16, 17, 18, 19, 20}
 
 	headerID := make([]int, 0)
 	headerID = append(array)
@@ -42,6 +42,9 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
 	rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
 	arr1Index := rand.Intn(len(headerID))
+
+	fmt.Println(time.Now().UTC())
+
 	//res := array2[arr1Index]
 	arr2Index := rand.Intn(len(fieldId[arr1Index]))
 	fmt.Println(headerID[arr1Index])
@@ -54,6 +57,7 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	context.SetOutput("headerID", headerID[arr1Index])
 	context.SetOutput("fieldID", fieldId[arr1Index][arr2Index])
 	context.SetOutput("alertType", arr3Index)
+	context.SetOutput("notificationTime", time.Now().UTC())
 
 	return true, nil
 }
