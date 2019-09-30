0827b642ffe201b2b7910776c7a570d4a4fe6205
shalini.n@isteer.com <shalini.n@isteer.com>
2019-09-30 18:30:19 +0530

    updated code

--- activity/influxdb/activity.go
+++ activity/influxdb/activity.go
@@ -4,7 +4,7 @@ import (
 	"log"
 
 	"github.com/project-flogo/core/activity"
-	_ "github.com/influxdata/influxdb-client" // this is important because of the bug in go mod
+	_ "github.com/influxdata/influxdb1-client" // this is important because of the bug in go mod
    client "github.com/influxdata/influxdb1-client/v2"
 )
 
