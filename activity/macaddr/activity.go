cf93cddca74ff37fe7e13c8f190d5a9d633d9114
shalini.n@isteer.com <shalini.n@isteer.com>
2019-11-06 12:51:59 +0530

    new activity

---
+++ activity/macaddr/activity.go
@@ -0,0 +1,68 @@
+package influxdb
+
+import (
+	"log"
+
+	"github.com/project-flogo/core/activity"
+)
+
+func init() {
+	_ = activity.Register(&Activity{}) //activity.Register(&Activity{}, New) to create instances using factory method 'New'
+}
+
+var activityMd = activity.ToMetadata(&Input{}, &Output{})
+
+//New optional factory method, should be used if one activity instance per configuration is desired
+func New(ctx activity.InitContext) (activity.Activity, error) {
+
+	act := &Activity{} //add aSetting to instance
+
+	return act, nil
+}
+
+// Activity is an sample Activity that can be used as a base to create a custom activity
+type Activity struct {
+}
+
+// Metadata returns the activity's metadata
+func (a *Activity) Metadata() *activity.Metadata {
+	return activityMd
+}
+
+func getMacAddr() ([]string, error) {
+    ifas, err := net.Interfaces()
+    if err != nil {
+        return nil, err
+    }
+    var as []string
+    for _, ifa := range ifas {
+        a := ifa.HardwareAddr.String()
+        if a != "" {
+            as = append(as, a)
+        }
+    }
+    return as, nil
+}
+// Eval implements api.Activity.Eval - Logs the Message
+func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
+
+
+	as, err := getMacAddr()
+    if err != nil {
+        log.Fatal(err)
+    }
+    //for _, a := range as {
+		out := as[1]
+
+	//log.Println("setting:", settings.ASetting)
+	//ctx.Logger().Debug("Output: %s", settings.ASetting)
+	//ctx.Logger().Debugf("Input: %s", input.AnInput)
+
+	output := &Output{Output: out}
+	err = ctx.SetOutputObject(output)
+	if err != nil {
+		return true, err
+	}
+
+	return true, nil
+}
