0c8c5bb216c14409c0a4e57fc12e384c64820625
shalini.n@isteer.com <shalini.n@isteer.com>
2019-11-15 00:14:48 +0530

    modifications

--- activity/hardware/metadata.go
+++ activity/hardware/metadata.go
@@ -19,6 +19,5 @@ func (o *Output) FromMap(values map[string]interface{}) error {
 func (o *Output) ToMap() map[string]interface{} {
 	return map[string]interface{}{
 		"Output": o.Output,
-		"macaddr":o.Macaddr,
 	}
 }
