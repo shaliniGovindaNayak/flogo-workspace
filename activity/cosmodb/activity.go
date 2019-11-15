790459d77c3046775dd797a8c7f771bf17c02e38
shalini.n@isteer.com <shalini.n@isteer.com>
2019-11-15 10:53:23 +0530

    resolved errors

--- activity/cosmodb/activity.go
+++ activity/cosmodb/activity.go
@@ -82,7 +82,7 @@ func insertdata(username string, url string, password string, content interface{
 	session.SetSafe(&mgo.Safe{})
 	collection := session.DB(database).C("details")
 
-	cont := cosmodb.Details(content)
+	cont := &Details(content)
 
 	// insert Document in collection
 	// insert Document in collection
