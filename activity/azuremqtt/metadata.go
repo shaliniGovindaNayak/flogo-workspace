27007fa27467dd588718e24a19c50b78e3458daa
shalini.n@isteer.com <shalini.n@isteer.com>
2019-11-28 12:58:48 +0530

    updated act

---
+++ activity/azuremqtt/metadata.go
@@ -0,0 +1,44 @@
+package azuremqtt
+
+import "github.com/project-flogo/core/data/coerce"
+
+type Input struct {
+	Content     string `md:"Content,required"`
+	Username    string `md:"Username,required"`
+	Password    string `md:"Password,required"`
+	Brokerurl string `md:"Brokerurl,required"`
+}
+
+type Output struct {
+	Output string `md:"Output"`
+}
+
+func (i *Input) FromMap(values map[string]interface{}) error {
+	//var err error
+	i.Content, _ = coerce.ToString(values["Content"])
+	i.Username, _ = coerce.ToString(values["Username"])
+	i.Password, _ = coerce.ToString(values["Password"])
+	i.Instanceurl, _ = coerce.ToString(values["Instanceurl"])
+	return nil
+}
+
+func (i *Input) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"Content":     i.Content,
+		"Username":    i.Username,
+		"Password":    i.Password,
+		"Instanseurl": i.Instanceurl,
+	}
+}
+
+func (o *Output) FromMap(values map[string]interface{}) error {
+	o.Output, _ = coerce.ToString(values["Output"])
+
+	return nil
+}
+
+func (o *Output) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"Output": o.Output,
+	}
+}
--- activity/hardware/activity.go
+++ activity/hardware/activity.go
@@ -131,7 +131,7 @@ func GetHardwareData() string{
 		}
 		
 		//fmt.Println(cpuarray)
-		cpudetails:=map[string]interface{}{"cpu1":cpuarray[0],"cpu2":cpuarray[1],"cpu3":cpuarray[2],"cpu4":cpuarray[3],}
+		//cpudetails:=map[string]interface{}{"cpu1":cpuarray[0],"cpu2":cpuarray[1],"cpu3":cpuarray[2],"cpu4":cpuarray[3],}
 		//fmt.Printf("unexpected type %T", cpudetails)
 		c, err := json.Marshal(cpudetails)
 		if err != nil {
@@ -205,7 +205,7 @@ func GetHardwareData() string{
 		"Ip_address":addr,
 		"mac_address":as[1],
 		"Operating_system":runtimeOS,
-		"current_cpu_utilization":cpudetails,
+		//"current_cpu_utilization":cpudetails,
 		"Total_memory": strconv.FormatUint(diskStat.Total, 10),
 		"Free_memory ": strconv.FormatUint(vmStat.Free, 10),
 		"Percentage_used_memory":strconv.FormatFloat(vmStat.UsedPercent, 'f', 2, 64),
