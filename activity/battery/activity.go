b602dac8cbe614ea12de3c547d00d7543d14a755
Shalini <shalini.n@isteer.com>
2020-02-26 13:06:53 +0530

    new activity added

---
+++ activity/battery/activity.go
@@ -0,0 +1,62 @@
+package battery
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/distatus/battery"
+
+	"github.com/project-flogo/core/activity"
+)
+
+func init() {
+	_ = activity.Register(&Activity{}) //activity.Register(&Activity{}, New) to create instances using factory method 'New'
+}
+
+var activityMd = activity.ToMetadata(&Output{})
+
+//New optional factory method, should be used if one activity instance per configuration is desired
+func New(ctx activity.InitContext) (activity.Activity, error) {
+
+	act := &Activity{} //add aSetting to instance
+
+	return act, nil
+}
+
+// Activity is an sample Activity that can be used as a base to create a custom activity
+type Activity struct {
+}
+
+// Metadata returns the activity's metadata
+func (a *Activity) Metadata() *activity.Metadata {
+	return activityMd
+}
+
+// Eval implements api.Activity.Eval - Logs the Message
+func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
+
+	batteries, err := battery.GetAll()
+	//fmt.Println(batteries[0])
+	if err != nil {
+		fmt.Println("Could not get battery info!")
+		return
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	//for _, a := range as {
+	out := batteries[0]
+	fmt.Println(out)
+	//log.Println("setting:", settings.ASetting)
+	//ctx.Logger().Debug("Output: %s", settings.ASetting)
+	//ctx.Logger().Debugf("Input: %s", input.AnInput)
+
+	output := &Output{Output: out}
+	err = ctx.SetOutputObject(output)
+	if err != nil {
+		return true, err
+	}
+
+	return true, nil
+}
---
+++ activity/battery/metadata.go
@@ -0,0 +1,20 @@
+package battery
+
+import "github.com/project-flogo/core/data/coerce"
+
+type Output struct {
+	Output string `md:"Output"`
+}
+
+func (o *Output) FromMap(values map[string]interface{}) error {
+	strVal, _ := coerce.ToString(values["Output"])
+	o.Output = strVal
+
+	return nil
+}
+
+func (o *Output) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"Output": o.Output,
+	}
+}
