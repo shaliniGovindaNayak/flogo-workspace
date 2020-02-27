55113ba1e207c1553e46f7c31d62f9e7d74957fa
Shalini <shalini.n@isteer.com>
2020-02-27 11:33:31 +0530

    updated return value

--- activity/battery/metadata.go
+++ activity/battery/metadata.go
@@ -1,13 +1,11 @@
 package battery
 
-import "github.com/project-flogo/core/data/coerce"
-
 type Output struct {
 	Output map[string]interface{} `md:"Output"`
 }
 
 func (o *Output) FromMap(values map[string]interface{}) error {
-	strVal, _ := coerce.ToString(values["Output"])
+	strVal, _ := values["Output"]
 	o.Output = strVal
 
 	return nil
